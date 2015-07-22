53da66e66021ccd536f2f7a568c5114619e04003
Khemarat Boonyapaluk <korla.march@gmail.com>
2015-07-22 09:56:40 +0700

    Update STEM.ino

--- C_C++_Dev/STEM/STEM.ino
+++ C_C++_Dev/STEM/STEM.ino
@@ -1,32 +1,41 @@
 #include <ipst.h>
 
-#define REF 700
+#define REF 600
 
 void fw2(int a, int b){
  motor(1,a);
  motor(2,b); 
 }
-void track(int n){
+void turnR90(){
+  /*fw2(70,70);
+  delay(10);*/
+  fw2(60,-60);
+  delay(150);
+  while(analog(3)>=REF);
+  fw2(-100,100);
+  delay(50);
+  motor_stop(ALL);
+}
+int i;
+void track(){
  if(analog(1)<REF){
-    fw2(-20,80);
+    fw2(-20,70);
     i = 3;
-    if(n==1) return;
   }else if(analog(4)<REF){ 
-    fw2(80,-20);
+    fw2(70,-20);
     i = 4;
-    if(n==4) return;
   }else if(analog(2)<REF&&analog(3)<REF){
       fw2(70,70);
   }else if(analog(2)>=REF&&analog(3)<REF){
-      fw2(80,50);
+      fw2(70,50);
       i = 1;
   }else if(analog(2)<REF&&analog(3)>=REF){
-      fw2(50,80);
+      fw2(50,70);
       i = 2;
   }else{
       switch(i){
-       case 1: fw2(80,20); break;
-        case 2: fw2( 20,80); break;
+       case 1: fw2(70,20); break;
+        case 2: fw2( 20,70); break;
         case 3: fw2(-50,50); break;
         case 4: fw2(50,-50); break;
       }
@@ -35,7 +44,21 @@ void track(int n){
 void setup(){
   OK();
 }
-int i;
-void loop(){
-  
+
+
+void loop()
+{
+  while(analog(4)>=REF) track();
+  turnR90();
+  while(analog(1)>=REF&&analog(4)>=REF) track();
+  fw2(55,55);
+  delay(500);
+  fw2(-80,-80);
+  delay(600);
+  fw2(100,100);
+  delay(50);
+  motor_stop(ALL);
+  OK();
+  glcd(0,0,"%d %d    ",analog(1),analog(2));
+  glcd(1,0,"%d %d    ",analog(3),analog(4));
 }

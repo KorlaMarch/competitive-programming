3e59f69b995d537b6b77ea2e984b70b9cc17a826
Khemarat Boonyapaluk <korla.march@gmail.com>
2015-12-16 15:06:52 +0700

    Update 1043_Power.c

--- C_C++_Dev/programming.in.th-MID_1_V2/1043_Power.c
+++ C_C++_Dev/programming.in.th-MID_1_V2/1043_Power.c
@@ -1,23 +1,48 @@
 #include "stdio.h"
 #include "math.h"
-double abs(double d){
-    return d<0.0?d*-1:d;
-}
-int pow(int base, int r){
-    if(r == 0) return 1;
-    return base*pow(r-1);
-}
-int binary_pow(int y, int k){
+int n,i,j,y,sq,p,g,c;
+char isP[10105];
+int prime[4000];
 
+int gcd(int a, int b){
+    int t;
+    if(a==0) return b;
+    else if(b==0) return a;
+    else{
+        if(a<=b){
+            t = a;
+            a = b;
+            b = t;
+        }
+        while(b){
+            t = b;
+            b = a%b;
+            a = t;
+        }
+        return a;
+    }
 }
+
 int main(){
-    int n,i,y,m,k;
+    prime[p++] = 2;
+    for(i = 3; i <= 10100; i += 2){
+        if(!isP[i]){
+            prime[p++] = i;
+            for(j = i+i; j <= 10100; j += i){
+                isP[j] = 1;
+            }
+        }
+    }
     scanf("%d",&n);
     for(i = 0; i < n; i++){
-        scanf("%d", &y);
-        for(m = 1; m<y; m *= 2);
-        for(k=m;k>1;k++){
-            if(binary_pow(y,k))
+        scanf("%d",&y);
+        for(j=g=0; y>1 && j<p; j++){
+            if(y%prime[j]==0){
+                for(c=0;y%prime[j]==0;c++,y/=prime[j]);
+                g = gcd(g,c);
+            }
         }
+        if(g<=1) printf("NO\n");
+        else printf("%d\n",g);
     }
 }

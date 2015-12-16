3ded69cafe3ebf5ccf6eb65afbdd9b0ba792e714
Khemarat Boonyapaluk <korla.march@gmail.com>
2015-12-16 11:33:45 +0700

    Update 1086_diary.c

--- C_C++_Dev/programming.in.th-MID_1_V2/1086_diary.c
+++ C_C++_Dev/programming.in.th-MID_1_V2/1086_diary.c
@@ -1,26 +1,26 @@
 #include "stdio.h"
-int n,k,a,i,j,c,mp;
-int page[100];
-char day[100001],read[100001];
+
+int n,k,a,i,j,x,p,c,mx,mxp;
+int day[10010];
 
 int main(){
     scanf("%d%d%d",&n,&k,&a);
-
-    for(i=c=0; i < n; i++){
-        scanf("%d", &page[i]);
-        for(j = 0; j < page[i]; j++,c++){
-            if(c-a>=0){
-                if(a>=j) day[c] = day[c-a]+1;
-                else day[c] = day[c-a];
-                read[c] = read[c-a]+1;
-            }else{
-                day[c] = read[c] = 1;
+    for(i=0,p=1; i < n; i++){
+        scanf("%d",&x);
+        for(j = 0; j < x; j++) day[p++] = i;
+    }
+    for(i = 1; i < p; i++){
+        for(j=c=0,x=-1; j < k&&i+a*j < p; j++){
+            if(day[i+a*j]!=x){
+                x = day[i+a*j];
+                c++;
             }
         }
+        if(j!=k) break;
+        if(c>mx){
+            mxp = i;
+            mx = c;
+        }
     }
-    for(i = 0, mp = -1; i < c; i++){
-        printf("%d:%d %d %d %d\n",i,day[i],read[i],i-a*k,mp-a*k);
-        if(read[i] >= k && (mp==-1||day[i]-day[i-a*k]>day[mp]-day[mp-a*k])) mp = i;
-    }
-    printf("%d %d\n",mp-a*(k-1)+1,day[mp]);
+    printf("%d %d\n",mxp,mx);
 }
